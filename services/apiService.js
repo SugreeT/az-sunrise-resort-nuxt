5df46a6af57a9d79dccfd1dbe5b1019faf8f7961
Sugree <Sugree.popo@gmail.com>
2025-06-07 22:34:59 +0700

    init config for prod

--- services/apiService.js
+++ services/apiService.js
@@ -1,36 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { useLoading } from '@/composables/useLoading'
+import { useLoading } from "@/composables/useLoading";
 
-const { start, done } = useLoading()
+const { start, done } = useLoading();
 
 const apiClient = axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: 'http://165.22.55.16:8080/',
-  withCredentials: true,    
+  // baseURL: 'http://165.22.55.16:8080/',
+  baseURL: "https://azsunriseresort.com/api/",
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     // 'x-access-key': 'X-ACCESS-KEY-VALUE-TEST',
-    'x-access-key': 'FKqDHmj767ywY9mUhTqeDjdFC',
-    
+    "x-access-key": "FKqDHmj767ywY9mUhTqeDjdFC",
   },
 });
 
 // ถ้ารูปเก็บบนโดเมนอื่น กำหนดแยก
-const IMAGE_BASE_URL = 'https://azsunriseresort.com/images';
+const IMAGE_BASE_URL = "https://azsunriseresort.com/images";
 
 // interceptor รันก่อน request
-apiClient.interceptors.request.use(config => {
-  start()
-  return config
-})
+apiClient.interceptors.request.use((config) => {
+  start();
+  return config;
+});
 
 // interceptor รันหลังได้ response หรือ error
 apiClient.interceptors.response.use(
-  res => { done(); return res },
-  err => { done(); return Promise.reject(err) }
-)
-
+  (res) => {
+    done();
+    return res;
+  },
+  (err) => {
+    done();
+    return Promise.reject(err);
+  }
+);
 
 const apiService = {
   async get(url, params = {}) {
@@ -55,25 +60,24 @@ const apiService = {
   },
 
   async callServiceInfo() {
-      if (process.client) {
-        const cached = sessionStorage.getItem("pageInfo");
-        if (cached) {
-          return JSON.parse(cached);
-        }
+    if (process.client) {
+      const cached = sessionStorage.getItem("pageInfo");
+      if (cached) {
+        return JSON.parse(cached);
       }
-      // ถ้ายังไม่มีใน sessionStorage → ดึงจาก API
-      const data = await this.get("/api/page-info/search");
-
-      // 3) เก็บลง sessionStorage (stringify จะได้เป็น JSON text)
-      if (process.client) {
-        if (data) {
-            sessionStorage.setItem("pageInfo", JSON.stringify(data));
-        }
-      
-      }
-      
-      return data;
     }
+    // ถ้ายังไม่มีใน sessionStorage → ดึงจาก API
+    const data = await this.get("/api/page-info/search");
+
+    // 3) เก็บลง sessionStorage (stringify จะได้เป็น JSON text)
+    if (process.client) {
+      if (data) {
+        sessionStorage.setItem("pageInfo", JSON.stringify(data));
+      }
+    }
+
+    return data;
+  },
 };
 
 export default apiService;
